5c7a827f502cdf3f677789b4103ea754b09eea98
Antonin SIMON <antonin.simon@epsi.fr>
2023-05-16 09:04:08 +0200

    stup swagger UI

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
     res.send('Hello world !!');

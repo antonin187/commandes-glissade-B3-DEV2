bc8ed4d855c3d66663ad9346df1872300f92b035
Antonin SIMON <antonin.simon@epsi.fr>
2023-05-16 10:16:27 +0200

    setup express api

--- server.js
+++ server.js
@@ -1,17 +1,85 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = process.env.PORT || 8000;
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.set('Content-Type', 'text/html');
-    res.send('Hello world !!');
+mongoose.connect(
+    "mongodb+srv://invite:guest@cluster0.o82caff.mongodb.net/Glissade?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+
+const commandeSchema = new mongoose.Schema({
+  date: String,
+  acheteur: {
+    nom: String,
+    email: String,
+    adresse: {
+      rue: String,
+      ville: String,
+      pays: String,
+      zipCode: String,
+    },
+    telephone: String,
+  },
+  adresseLivraison: {
+    rue: String,
+    ville: String,
+    pays: String,
+    zipCode: String,
+  },
+  articles: [
+    {
+      reference: String,
+      nom: String,
+      description: String,
+      prix: Number,
+      quantité: Number,
+      etat: String,
+      vendeur: {
+        nom: String,
+        email: String,
+        telephone: String,
+      },
+    },
+  ],
+  totalHT: Number,
+  fraisDePort: Number,
+  reduction: Number,
+  taxe: Number,
+  totalTTC: Number,
+  modePaiement: String,
+  statut: String,
+});
+
+const Commandes = mongoose.model("Commandes", commandeSchema);
+
+// app.get("/", (req, res) => {
+//   res.set("Content-Type", "text/html");
+//   res.send("Hello world !!");
+// });
+
+app.get("/commandes", async (req, res) => {
+  try {
+    const commandes = await Commandes.find({});
+      res.send(commandes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 });
 
 app.listen(port, () => {
-    console.log('Server app listening on port ' + port);
+  console.log("Server app listening on port " + port);
 });
